e4b90182f0e4bbea843db9c5e5caef130e4a9ad0
Glenn Vriesman <glenn.vriesman@gmail.com>
2020-06-01 23:35:23 +0200

    feat: read sqlite db
    Signed-off-by: Glenn Vriesman <glenn.vriesman@gmail.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/glvr182/f2b-exporter
 
 go 1.14
 
-require github.com/sirupsen/logrus v1.6.0
+require (
+	github.com/alicebob/sqlittle v1.4.1-0.20200506090143-e4b415246cd8
+	github.com/sirupsen/logrus v1.6.0
+)
--- main.go
+++ main.go
@@ -1,8 +1,47 @@
 package main
 
-import log "github.com/sirupsen/logrus"
+import (
+	"github.com/alicebob/sqlittle"
+	log "github.com/sirupsen/logrus"
+)
+
+type prisoner struct {
+	jail      string
+	ip        string
+	timeofban int
+	bantime   int
+}
 
 func main() {
 	log.Println("Starting exporter")
+	db, err := sqlittle.Open("/var/lib/fail2ban/fail2ban.sqlite3")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
+	prisoners, err := jailed(db)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("%v", prisoners)
+}
+
+func jailed(db *sqlittle.DB) ([]prisoner, error) {
+	var (
+		prisoners []prisoner
+		p         prisoner
+	)
+
+	db.Select("bans", func(r sqlittle.Row) {
+		err := r.Scan(&p.jail, &p.ip, &p.timeofban, &p.bantime)
+		if err != nil {
+			panic(err)
+		}
+		prisoners = append(prisoners, p)
+
+	}, "jail", "ip", "timeofban", "bantime")
+
+	return prisoners, nil
 }
